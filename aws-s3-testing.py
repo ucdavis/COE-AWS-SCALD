53e28d148356bbdb7a16ced2b298dd0c1e6fcd02
deanbunn <deanbunn@gmail.com>
2025-12-29 14:16:13 -0800

    20251229_1415hrs DynamoDB projectiontype include

--- aws-s3-testing.py
+++ aws-s3-testing.py
@@ -1,13 +1,19 @@
 """ 
-Script: aws-testing.py
+Title: aws-s3-testing.py
+Authors: Dean Bunn and Ben Clark
+Last Edit: 2025-12-29
 """
 
 import boto3
 
+#Create AWS Session Using Specific Profile (Not Default)
 session = boto3.Session(profile_name="deanbunn",region_name="us-west-2")
 
+#Initiate S3 Session
 s3 = session.resource('s3')
 
+#View All Buckets
 for bucket in s3.buckets.all():
     print(bucket.name)
+
 
